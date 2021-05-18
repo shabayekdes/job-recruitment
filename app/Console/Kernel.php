1529e40b86efcc2b474e59b5d8ce69d8272f62c9
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-05-18 23:45:48 +0200

    Add careerjet job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,6 +29,17 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('job:wuzzuf')->dailyAt('13:00');
         $schedule->command('job:cantalop')->dailyAt('14:00');
+        $schedule->command('job:careerjet egypt --hour=15  --minute=4')->everyFiveMinutes();
+        $schedule->command('job:careerjet uae --hour=15  --minute=4')->everyFiveMinutes();
+        $schedule->command('job:careerjet kuwait --hour=15  --minute=4')->everyFiveMinutes();
+        $schedule->command('job:careerjet qatar --hour=15  --minute=4')->everyFiveMinutes();
+        $schedule->command('job:careerjet oman --hour=15  --minute=4')->everyFiveMinutes();
+        $schedule->command('job:careerjet algérie --hour=15  --minute=4')->everyFiveMinutes();
+        $schedule->command('job:careerjet saudi --hour=15  --minute=4')->everyFiveMinutes();
+        $schedule->command('job:careerjet morocco --hour=15  --minute=4')->everyFiveMinutes();
+        $schedule->command('job:careerjet libya --hour=15  --minute=4')->everyTenMinutes();
+        $schedule->command('job:careerjet tunisia --hour=15  --minute=4')->everyTenMinutes();
+
 
     }
 
