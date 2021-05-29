9980054bce1b029bbf4c67eeb34cda45940bd72e
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-05-29 05:25:56 +0200

    Disable in config database

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
                 'use_single_transaction',
                 'timeout' => 60 * 5, // 5 minute timeout
                 // 'exclude_tables' => ['table1', 'table2'],
-                'add_extra_option' => '--optionname=optionvalue',
+                // 'add_extra_option' => '--optionname=optionvalue',
             ]
         ],
 
