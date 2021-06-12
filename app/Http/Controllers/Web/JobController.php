05e212f6689c368799965d6a735f7465e694e4dc
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-06-12 19:58:08 +0000

    order sector by jobs count

--- app/Http/Controllers/Web/JobController.php
+++ app/Http/Controllers/Web/JobController.php
@@ -34,18 +34,19 @@ class JobController extends Controller
         $filter[] = request()->query('industry');
         $filter[] = request()->query('qualifications');
 
-        $terms = Term::withCount(['jobs' => function($query){
-                            $query->where('post_type', 'job_listing');
-                        }])
-                        ->whereHas('jobs', function($query){
-                            $query->where('post_type', 'job_listing');
-                        })
-                        ->whereHas('taxonomy', function($query){
-                            $query->where('taxonomy', 'sector');
-                        })
-                        ->limit(20)
-                        // ->having('jobs_count', '>', 0)
-                        ->get();
+        $terms = Term::withCount(['jobs' => function ($query) {
+            $query->where('post_type', 'job_listing');
+        }])
+            ->whereHas('jobs', function ($query) {
+                $query->where('post_type', 'job_listing');
+            })
+            ->whereHas('taxonomy', function ($query) {
+                $query->where('taxonomy', 'sector');
+            })
+            // ->limit(20)
+            ->orderByDesc('jobs_count')
+            // ->having('jobs_count', '>', 0)
+            ->get();
 
         // $types = Term::whereHas('taxonomy', function ($query) {
         //                     $query->where('taxonomy', 'jobtype');
@@ -54,49 +55,49 @@ class JobController extends Controller
         // dd($terms);
 
         $jobs = Job::with([
-                    'meta',
-                    'term' => function($query) use($term){
-                        $query->whereIn('term_id', $term);
-                    }
-                    ])
-                    ->whereHas('meta', function($query) use($career){
-                        if(request()->has('career-level')){
-                            $query->where('meta_value', 'LIKE' , "%{$career}%");
-                        }
-                    });
+            'meta',
+            'term' => function ($query) use ($term) {
+                $query->whereIn('term_id', $term);
+            }
+        ])
+            ->whereHas('meta', function ($query) use ($career) {
+                if (request()->has('career-level')) {
+                    $query->where('meta_value', 'LIKE', "%{$career}%");
+                }
+            });
 
-        if(request()->has('term')){
-            $jobs->whereHas('term', function($query) use($term){
+        if (request()->has('term')) {
+            $jobs->whereHas('term', function ($query) use ($term) {
                 $query->whereIn('term_id', $term);
             });
         }
 
         if (count(array_filter($filter)) > 0) {
-            $jobs->whereHas('meta', function($query) use($filter){
+            $jobs->whereHas('meta', function ($query) use ($filter) {
                 $query->whereIn('meta_value', array_filter($filter));
             });
         }
 
-        if(request()->has('date')){
+        if (request()->has('date')) {
             $jobs->whereBetween('post_date', [Carbon::parse($date), now()]);
             // dd(Carbon::parse($date));
         }
 
-        if(request()->has('search')){
+        if (request()->has('search')) {
 
-            $jobs->where(function ($query) use ($search){
-                    // subqueries goes here
-                    $query->where('post_title', 'LIKE', "%{$search}%");
-                });
+            $jobs->where(function ($query) use ($search) {
+                // subqueries goes here
+                $query->where('post_title', 'LIKE', "%{$search}%");
+            });
         }
 
         $jobs = $jobs->where(function ($query) {
-                        // subqueries goes here
-                        $query->where('post_type', 'LIKE', "job_listing")
-                            ->orWhere('post_type', 'LIKE', "job");
-                    })
-                    ->orderByDesc('post_date')
-                    ->paginate()->onEachSide(1);
+            // subqueries goes here
+            $query->where('post_type', 'LIKE', "job_listing")
+                ->orWhere('post_type', 'LIKE', "job");
+        })
+            ->orderByDesc('post_date')
+            ->paginate()->onEachSide(1);
 
         // dd($jobs->toSql());
         return view('web.job.index', compact('jobs', 'terms', 'locations'));
@@ -112,14 +113,14 @@ class JobController extends Controller
     {
         $job = Job::with('term', 'meta')->findOrFail($id);
 
-        if($jobSlug == null){
+        if ($jobSlug == null) {
             return redirect()->route('job.show', ['id' => $id, 'job' => $job->post_name]);
         }
 
         $relatedJobs = Job::with([
-                'term',
-                'meta'
-            ])
+            'term',
+            'meta'
+        ])
             ->where('post_type', 'job_listing')
             ->limit(5)
             ->inRandomOrder()
@@ -133,7 +134,6 @@ class JobController extends Controller
 
 
         return view('web.job.show', compact('job', 'relatedJobs'));
-
     }
 
     /**
@@ -160,69 +160,68 @@ class JobController extends Controller
 
         $candidate = Job::where('post_type', 'candidate')->where('post_author', $candidateId)->first();
 
-        if($candidate == null){
+        if ($candidate == null) {
             return redirect()->route('job.index');
         }
 
 
-        if($request->get('cover_letter') != null){
+        if ($request->get('cover_letter') != null) {
             $jobCvrs = JobMeta::where('post_id', $id)
-                    ->where('meta_key', 'jobsearch_job_apply_cvrs')
-                    ->first();
+                ->where('meta_key', 'jobsearch_job_apply_cvrs')
+                ->first();
 
-            if($jobCvrs){
+            if ($jobCvrs) {
                 $cvrs = unserialize($jobCvrs->meta_value);
                 // if (array_key_exists($candidate->ID, $cvrs)) {
-                    $cvrs[$candidate->ID] = $request->get('cover_letter');
-                    $jobCvrs->update(['meta_value' => serialize($cvrs)]);
+                $cvrs[$candidate->ID] = $request->get('cover_letter');
+                $jobCvrs->update(['meta_value' => serialize($cvrs)]);
                 // }
 
-            }else {
+            } else {
                 $cvrs = [$candidate->ID => $request->get('cover_letter')];
 
                 $jobCvrs = JobMeta::create([
-                            'post_id' => $id,
-                            'meta_key' => 'jobsearch_job_apply_cvrs',
-                            'meta_value' => serialize($cvrs)
-                        ]);
+                    'post_id' => $id,
+                    'meta_key' => 'jobsearch_job_apply_cvrs',
+                    'meta_value' => serialize($cvrs)
+                ]);
             }
         }
 
         $jobApplicants = JobMeta::where('post_id', $id)
-                            ->where('meta_key', 'jobsearch_job_applicants_list')
-                            ->first();
+            ->where('meta_key', 'jobsearch_job_applicants_list')
+            ->first();
 
-        if($jobApplicants){
+        if ($jobApplicants) {
             $applicants = explode(",", $jobApplicants->meta_value);
-            if(!array_search($candidate->ID , $applicants)){
+            if (!array_search($candidate->ID, $applicants)) {
                 $applicants[] = $candidate->ID;
                 $jobApplicants->update(['meta_value' => implode(",", $applicants)]);
             }
-        }else {
+        } else {
             $cvrs = [$candidate->ID => $request->get('cover_letter')];
 
             $jobCvrs = JobMeta::create([
-                        'post_id' => $id,
-                        'meta_key' => 'jobsearch_job_applicants_list',
-                        'meta_value' => $candidate->ID
-                    ]);
+                'post_id' => $id,
+                'meta_key' => 'jobsearch_job_applicants_list',
+                'meta_value' => $candidate->ID
+            ]);
         }
 
         $candidateMeta = CandidateMeta::where('user_id', $candidateId)
-                                        ->where('meta_key', 'jobsearch-user-jobs-applied-list')
-                                        ->first();
+            ->where('meta_key', 'jobsearch-user-jobs-applied-list')
+            ->first();
 
-        if($candidateMeta){
+        if ($candidateMeta) {
             $candidateMetaAppliedList = unserialize($candidateMeta->meta_value);
 
-            if(!array_search($id , $candidateMetaAppliedList)){
+            if (!array_search($id, $candidateMetaAppliedList)) {
                 $candidateMetaAppliedList[] = [
                     'post_id' => $id,
                     'date_time' => time()
                 ];
                 $candidateMeta->update(['meta_value' => serialize($candidateMetaAppliedList)]);
             }
-
         }
 
 
