779d6f800b8838702f2772b7ebb6ebd8105dc0d8
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-07-10 22:31:56 +0200

    Fix monster command pubDate key

--- app/Console/Commands/Monster.php
+++ app/Console/Commands/Monster.php
@@ -67,7 +67,7 @@ class Monster extends Command
 
             $encryption = openssl_encrypt($job['title'], "AES-128-CTR", "Monster", 0, "1234567891011121");
             // $decryption = openssl_decrypt($encryption, "AES-128-CTR", "GulfTalent", 0, "1234567891011121");
-            $job_id = date("Y-m-d", strtotime($job['pubDate'])) . '-' . $encryption;
+            $job_id = date("Y-m-d", time()) . '-' . $encryption;
 
             $jobExists = JobMeta::where('meta_value', $job_id)->exists();
 
